880dd03415f9868ce89ae6342269f2b18be21180
Thanyakorn63130500056 <tanyakon.pleng@gmail.com>
2022-04-27 17:45:12 +0700

    style event list

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -11,18 +11,32 @@
     <div>
         <ul>
             <li v-for="(event,index) in eventList" :key="index">
-                นัดหมายที่ {{event.bookingId}}<br>
-                คลีนิก : {{event.eventCategory}}<br>
-                ชื่อผู้นัดหมาย : {{event.bookingName}}<br>
-                Email : {{event.bookingEmail}}<br>
-                รายละเอียด : {{event.eventNotes}}<br>
-                วันที่ - เวลานัดหมาย : {{event.eventStartTime}}<br>
-                ระยะเวลา : {{event.eventDuration}}
+                <p><b>นัดหมายที่ {{event.bookingId}}</b></p>
+            <div class="card-body-main">
+                <div class="card" style="width: 45rem;">
+                    <div class="card-body">
+                        <b>วันที่ - เวลานัดหมาย :</b> {{event.eventStartTime}}<br>
+                        <b>ระยะเวลา :</b> {{event.eventDuration}} นาที <br>
+                        <b>คลีนิก :</b> {{event.eventCategory}}<br>
+                        <b>ชื่อผู้นัดหมาย :</b> {{event.bookingName}}        
+                    </div>
+                </div>    
+            </div>         
             </li>
         </ul>
     </div>
 </template>
 
 <style scoped>
-
+.card-body-main{
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+}
+ul{
+    list-style-type: none;
+}
+p{
+    margin-left: 30%;
+}
 </style>

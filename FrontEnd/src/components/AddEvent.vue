a8e698e9001447bef1c4fb29ae0fd8d7c1c5d347
Tarathep63130500055 <tarathep.siripis@mail.kmutt.ac.th>
2022-05-14 15:10:22 +0700

    ีupdate [frontend,backend] ready to deploy

--- FrontEnd/src/components/AddEvent.vue
+++ FrontEnd/src/components/AddEvent.vue
@@ -4,9 +4,9 @@ import ManageAdd from './ManageAdd.vue'
 
 const categories = ref([])
 const getEventCategory = async () => {
-    // const res = await fetch(`${import.meta.env.VITE_BASE_URL}eventcategory`)
+    const res = await fetch(`${import.meta.env.VITE_BASE_URL}eventcategory`)
     // const res = await fetch('http://10.4.56.123:8080/api/eventcategory')
-    const res = await fetch('http://localhost:8080/api/eventcategory')
+    // const res = await fetch('http://localhost:8080/api/eventcategory')
     if (res.status === 200) {
         categories.value = await res.json()
     }
@@ -18,9 +18,9 @@ onBeforeMount(async () => {
 const events = ref([])
 
 const getEvents = async () => {
-    // const res = await fetch(`${import.meta.env.VITE_BASE_URL}events` ,{
+    const res = await fetch(`${import.meta.env.VITE_BASE_URL}events` ,{
     // const res = await fetch(`http://10.4.56.123:8080/api/events/`, {
-    const res = await fetch(`http://localhost:8080/api/events`, {
+    // const res = await fetch(`http://localhost:8080/api/events`, {
         method: "GET",
     });
     if (res.status === 200) {

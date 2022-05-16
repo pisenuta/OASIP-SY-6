5c660ba06185c927212b37f111b23dddfa634dc8
Thanyakorn63130500056 <tanyakon.pleng@gmail.com>
2022-05-17 03:19:12 +0700

    update [frontend] all clinic

--- FrontEnd/src/components/Clinic.vue
+++ FrontEnd/src/components/Clinic.vue
@@ -11,36 +11,22 @@ const getEventCategory = async () => {
 onBeforeMount(async () => {
     await getEventCategory()
 })
+// v-for="(category, index) in categories" :key="index"
+//                 :value="category"
 </script>
  
 <template>
     <div class="body">
         <h3 class="mx-auto mt-5" style="font-size: 40px;font-weight: bolder;">Clinic</h3>
-        <div class="containerForCard" v-for="(category, index) in categories" :key="index" :value="category">
-            <div class="cardDetail">
-                <div class="face face1">
-                    <div class="content">
-                        <div v-if="category.id === 1">
-                            <img src="https://api.iconify.design/carbon/id-management.svg?color=black">
+        <div class="containerClinic mt-5">
+            <div class="row">
+                <div class="col-4" v-for="(category, index) in categories" :key="index" :value="category">
+                    <div class="card-body">
+                        <h5 class="card-title">{{category.eventCategoryName}}</h5>
+                        <div v-if="category.eventCategoryDescription === null || category.eventCategoryDescription === ''">
+                            <p>No Description.</p>
                         </div>
-                        <div v-if="category.id === 2">
-                            <img src="https://api.iconify.design/carbon/id-management.svg?color=black">
-                        </div>
-                        <div v-if="category.id === 3">
-                            <img src="https://api.iconify.design/fa6-solid/database.svg?color=black">
-                        </div>
-                        <div v-if="category.id === 4">
-                            <a href="https://www.flaticon.com/free-icons/development" title="development icons"></a>
-                        </div>
-                        <div v-if="category.id === 5">
-                            <img src="https://api.iconify.design/carbon/id-management.svg?color=black">
-                        </div>
-                        {{category.eventCategoryName}}
-                    </div>
-                </div>
-                <div class="face face2">
-                    <div class="content">
-                        {{category.eventCategoryDescription}}
+                        <p class="card-text">{{category.eventCategoryDescription}}</p>
                     </div>
                 </div>
             </div>
@@ -49,4 +35,23 @@ onBeforeMount(async () => {
 </template>
  
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter&family=Noto+Sans+Thai&display=swap');
+
+.body{
+    font-family: 'Inter','Noto Sans Thai';
+}
+.card-body{
+    background-color: #212529;
+    border-radius: 10px;
+    height: 10rem;
+    margin-bottom: 20px;
+    width: 35rem;
+}
+.card-body h5{
+    color:#e74694;
+    font-weight: bold;
+}
+.card-body p{
+    color:white;
+}
 </style>

3fbf7822c03d13a0c75cc5644b3a98aeaebe6cf2
Tarathep63130500055 <tarathep.siripis@mail.kmutt.ac.th>
2022-05-18 23:41:18 +0700

    update [frontend] test change vite config

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -5,16 +5,16 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()], 
   base: '/sy6/',
-  // server: {
-  //   proxy: {
-  //     '/api' : {
-  //       target: 'http://intproj21.sit.kmutt.ac.th',
-  //       // target: 'http://localhost:8080',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     }
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/api' : {
+        target: 'http://intproj21.sit.kmutt.ac.th',
+        // target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
 
 

9a6dce3a7be5b4d0f49c9d998a802321c8e05eae
Thanyakorn63130500056 <tanyakon.pleng@gmail.com>
2022-05-21 03:27:13 +0700

    add [frontend] filter form

--- FrontEnd/src/components/ShowAllEvent.vue
+++ FrontEnd/src/components/ShowAllEvent.vue
@@ -1,93 +1,110 @@
 <script setup>
-  import {ref, onBeforeMount} from 'vue'
-  import EventList from './EventList.vue'
-  const events = ref([])
+import { ref, onBeforeMount } from 'vue'
+import EventList from './EventList.vue'
+import moment from 'moment';
+const events = ref([])
 
-  const getEvents= async () =>{ 
-    // const res = await fetch(`${import.meta.env.VITE_BASE_URL}events/` ,{ 
-    // const res = await fetch(`http://10.4.56.123:8080/api/events/` ,{ 
-    const res = await fetch(`http://localhost:8080/api/events/` ,{
+const getEvents = async () => {
+  // const res = await fetch(`${import.meta.env.VITE_BASE_URL}events/` ,{ 
+  // const res = await fetch(`http://10.4.56.123:8080/api/events/` ,{ 
+  const res = await fetch(`http://localhost:8080/api/events/`, {
     method: "GET",
   });
 
-      if(res.status === 200){
-        events.value = await res.json()
-      } else{
-        console.log('can not');
-      }
+  if (res.status === 200) {
+    events.value = await res.json()
+  } else {
+    console.log('can not');
   }
+}
 
-  const removeEvent = async (removeEventId) =>{
-      // const res = await fetch(`${import.meta.env.VITE_BASE_URL}events/${removeEventId}`,{method: 'DELETE'})
-      // const res = await fetch(`http://10.4.56.123:8080/api/events/${removeEventId}`,{method: 'DELETE'})
-      const res = await fetch(`http://localhost:8080/api/events/${removeEventId}`,{method: 'DELETE'})
-      if(res.status === 200) {
-        events.value = events.value.filter((event) => event.id !== removeEventId)
-        console.log('deleted successfully')
-      }
-      else console.log('error, can not delete')
+const removeEvent = async (removeEventId) => {
+  // const res = await fetch(`${import.meta.env.VITE_BASE_URL}events/${removeEventId}`,{method: 'DELETE'})
+  // const res = await fetch(`http://10.4.56.123:8080/api/events/${removeEventId}`,{method: 'DELETE'})
+  const res = await fetch(`http://localhost:8080/api/events/${removeEventId}`, { method: 'DELETE' })
+  if (res.status === 200) {
+    events.value = events.value.filter((event) => event.id !== removeEventId)
+    console.log('deleted successfully')
   }
+  else console.log('error, can not delete')
+}
 
-  const editEvent = async (editEvent) => {
-    // const res = await fetch(`${import.meta.env.VITE_BASE_URL}/events/${editEvent.id}`,{
-    const res = await fetch(`http://10.4.56.123:8080/api/events/${editEvent.id}`,{
-    // const res = await fetch(`http://localhost:8080/api/events/${editEvent.id}`,{
-      method: 'PUT',
-      headers:{
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-          eventStartTime: editEvent.eventStartTime,
-          eventNotes: editEvent.eventNotes
-      })
+const editEvent = async (editEvent) => {
+  // const res = await fetch(`${import.meta.env.VITE_BASE_URL}/events/${editEvent.id}`,{
+  // const res = await fetch(`http://10.4.56.123:8080/api/events/${editEvent.id}`,{
+  const res = await fetch(`http://localhost:8080/api/events/${editEvent.id}`, {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      eventStartTime: editEvent.eventStartTime,
+      eventNotes: editEvent.eventNotes
     })
-    if(res.status === 200){
-      console.log('edited successfully');
-    } else {
-      console.log('can not edit');
-    }
-  }
-
-  onBeforeMount(async () =>{
-    await getEvents();
   })
-
-  const schedule = () =>{
-    if(events.value.length == 0){
-      return "No Scheduled Event"
-    }
+  if (res.status === 200) {
+    console.log('edited successfully');
+  } else {
+    console.log('can not edit');
   }
+}
 
-  // const byDate = (a,b) => {
-  //   return new Date(b.eventStartTime).valueOf() - new Date(a.eventStartTime).valueOf();
-  // }
+onBeforeMount(async () => {
+  await getEvents();
+})
+
+const schedule = () => {
+  if (events.value.length == 0) {
+    return "No Scheduled Event"
+  }
+}
+
+const byPast = events.value.filter((e) => moment().isBefore(e.eventStartTime))
+const byUpcoming = events.value.filter((e) => moment().isAfter(e.eventStartTime))
+
+console.log(byUpcoming);
+console.log(byPast);
+console.log(moment());
 
 </script>
  
 <template>
-    <div class="body">
-        <h3 class="mx-auto mt-5" style="font-size: 40px;font-weight: bolder;">Schedule</h3>
-        <div v-if="events.length>8" class="scroll-down"></div>
-        <h5 class="mt-5">{{schedule()}}</h5>
-        <div v-if="events.length !== 0">
-          <EventList :eventList="events" @delete="removeEvent" @edit="editEvent"/>
-        </div>
-        
+  <div class="body">
+    <h3 class="mx-auto mt-5" style="font-size: 40px;font-weight: bolder;">Schedule</h3>
+    <div v-if="events.length > 8" class="scroll-down"></div>
+    <select class="form-select filter-form  mt-4" aria-label="Default select example">
+      <option selected>All</option>
+      <option value="upcoming">Upcoming</option>
+      <option value="past">Past</option>
+    </select>
+
+    <h5 class="mt-4">{{ schedule() }}</h5>
+    <div v-if="events.length !== 0">
+      <EventList :eventList="events" @delete="removeEvent" @edit="editEvent" />
     </div>
+
+  </div>
 </template>
  
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter&family=Noto+Sans+Thai&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Radio+Canada:wght@600&display=swap');
-.body{
-    font-family: 'Radio Canada','Noto Sans Thai';
+
+.body {
+  font-family: 'Radio Canada', 'Noto Sans Thai';
+}
+.filter-form{
+  width: 8rem;
+  margin:auto;
 }
 
-h5,h3{
-    text-align: center;
+h5,
+h3 {
+  text-align: center;
 }
-h5{
-    color: #646464;
+
+h5 {
+  color: #646464;
 }
 
 .scroll-down {
@@ -102,6 +119,7 @@ h5{
   transform: translate(2500%, 0);
 
 }
+
 .scroll-down::before,
 .scroll-down::after {
   content: "";
@@ -116,6 +134,7 @@ h5{
   border-left: transparent;
   animation: scroll-down 1s ease-in-out infinite;
 }
+
 .scroll-down::before {
   top: 30%;
   animation-delay: 0.3s;
@@ -127,12 +146,15 @@ h5{
     /* top:20%; */
     opacity: 0;
   }
+
   30% {
     opacity: 1;
   }
+
   60% {
     opacity: 1;
   }
+
   100% {
     top: 90%;
     opacity: 0;

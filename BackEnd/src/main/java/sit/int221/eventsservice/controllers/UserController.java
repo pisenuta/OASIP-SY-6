85eb4aa3e81eb41c83dc4fdac7d6cf65ce1babaa
Tarathep63130500055 <tarathep.siripis@mail.kmutt.ac.th>
2022-08-18 02:15:08 +0700

    add [backend] get user by id

--- BackEnd/src/main/java/sit/int221/eventsservice/controllers/UserController.java
+++ BackEnd/src/main/java/sit/int221/eventsservice/controllers/UserController.java
@@ -3,6 +3,7 @@ package sit.int221.eventsservice.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sit.int221.eventsservice.dtos.UserDTO;
@@ -26,5 +27,10 @@ public class UserController {
     @GetMapping({""})
     public List<UserDTO> getUsers(){
         return this.userService.getAllUser();
+    }
+
+    @GetMapping({"/{Id}"})
+    public UserDTO getUserByI(@PathVariable Integer Id){
+        return this.userService.getUserById(Id);
     }
 }
--- BackEnd/src/main/java/sit/int221/eventsservice/services/UserService.java
+++ BackEnd/src/main/java/sit/int221/eventsservice/services/UserService.java
@@ -3,8 +3,11 @@ package sit.int221.eventsservice.services;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import sit.int221.eventsservice.dtos.UserDTO;
+import sit.int221.eventsservice.entities.User;
 import sit.int221.eventsservice.repositories.UserRepository;
 
 import java.util.List;
@@ -24,5 +27,12 @@ public class UserService {
 
     public List<UserDTO> getAllUser(){
         return this.listMapper.mapList(this.repository.findAll(Sort.by("name").descending()), UserDTO.class, this.modelMapper);
+    }
+
+    public UserDTO getUserById(Integer id){
+        User user = this.repository.findById(id).orElseThrow(()-> {
+            return new ResponseStatusException(HttpStatus.NOT_FOUND, id + " Does Not Exist !!!");
+        });
+        return this.modelMapper.map(user , UserDTO.class);
     }
 }

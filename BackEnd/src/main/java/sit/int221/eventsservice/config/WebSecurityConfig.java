1fc78a3510f112e44cbfc46d44b9418929844b6d
Tarathep63130500055 <tarathep.siripis@mail.kmutt.ac.th>
2022-09-04 18:40:12 +0700

    update [backend] cors webconfig and comment cross origin

--- BackEnd/src/main/java/sit/int221/eventsservice/config/WebSecurityConfig.java
+++ BackEnd/src/main/java/sit/int221/eventsservice/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import sit.int221.eventsservice.filters.JwtRequestFilter;
 
 @Configuration
@@ -51,13 +52,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
-                // dont authenticate this particular request
+        httpSecurity.csrf().disable().cors().and()
                 .authorizeRequests().antMatchers("/api/login").permitAll().
-                // all other requests need to be authenticated
                         anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 

572c355c30187ae8649c456b9298dc500e1c5cdc
Thanyakorn63130500056 <tanyakon.pleng@gmail.com>
2022-10-06 03:59:19 +0700

    update [frontend] home page

--- FrontEnd/src/components/Home.vue
+++ FrontEnd/src/components/Home.vue
@@ -1,32 +1,34 @@
-<script setup></script>
+<script setup>
+  const token = localStorage.getItem("token");
+</script>
 
 <template>
   <div class="homepage">
-    <!-- <div class="title">
-      <h1><p>OASIP-SY-6</p> Online Appointment Scheduling</h1>
-    </div>
-      <img src="../assets/home.png" class="main">
-    <div class="caption">
-      <h2>
-        The website for Booking Appointment is easy to use. Just clicks 
-        <router-link to="/add-event"><a>Add Event.</a></router-link>
-      </h2>
-    </div> -->
     <div class="container-home">
       <div class="row">
-        <div class="col-md-6 align-self-center" style="text-align: center">
+        <div class="col-md-6 align-self-center" style="text-align: center; margin-left: -2vw;margin-right: -2vw;">
           <div class="inside"> 
-            <h1><b style="color: #f857a6;">OASIP-SY-6</b><br/>Online Appointment Scheduling</h1>
+            <h1 class="home-text"><b style="color: #f857a6;">OASIP-SY-6</b><br/>Online Appointment <br/>Scheduling</h1>
+            <table style="margin: auto; border-collapse: collapse;">
+              <tr style="font-size: 0.95vw; font-family: Montserrat; border-left: 2px solid; border-color: #f857a6;">
+                <th style="padding-left: 0.5vw;">Online Appointment Scheduling System for Integrated Project Clinics</th>
+              </tr>
+            </table>
             <router-link to="/login">
-              <button class="btn btn-primary btn-home-login" type="button">LogIn</button>
+              <button class="btn btn-primary btn-home-login" type="button">get started</button>
+            </router-link>          
+            <div v-if="token !== null">
+              <router-link to="/booking">
+              <button class="btn btn-primary btn-home-login" type="button">Booking</button>
             </router-link>
+            </div> 
           </div>
           
         </div>
 
         <div class="col-md-6">
-          <div class="inside">
-            <img src="../assets/home-background.png" style="width:30vw;" />
+          <div class="inside home-img" style="text-align: left;margin-left: -4vw;">
+            <img src="../assets/home4.png" style="width:50vw;" />
           </div>
           
         </div>
@@ -40,16 +42,39 @@
 @import url('https://fonts.googleapis.com/css2?family=Radio+Canada:wght@600&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Mitr&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
 .homepage {
     font-family: 'Mitr', sans-serif;
     text-align: center;
     justify-content: center;
 }
 
+
+.home-img{
+  animation: mymove infinite;
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+@keyframes mymove {
+  0% {
+    margin-top: -0.5vw;
+  }
+  100% {
+    margin-top: 1vw;
+  }
+}
+.home-text{
+  font-size: 3vw;
+  letter-spacing: 0.15vw;
+}
 .btn-home-login{
   /* display: block; */
   /* position: relative;  */
   margin-right: 9vw;
+  margin-top: 1vw;
   margin-left: 17%;
   font-size: 1vw;
   background-image: linear-gradient(to right, #f857a6 0%, #ff5858 51%, #f857a6 100%);
@@ -59,6 +84,8 @@
   background-size: 200% auto;
   color: white;
   border-color: transparent;
+  border-radius: 1.5vw;
+  letter-spacing: 0.2vw;
 }
 
 .btn-home-login:hover {
